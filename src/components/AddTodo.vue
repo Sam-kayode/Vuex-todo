7217a733e7b7251477121ffee069c6fec346ec91
Sam-kayode <marvelloussamkayode@gmail.com>
2022-02-14 23:00:00 +0100

    add event removal from actions

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,26 +2,34 @@
   <div>
     <h3>Add Todo</h3>
     <div class="add">
-      <form >
-        <input type="text" v-model="title" placeholder="Add Todo....">
-        <input type="submit" value="submit">
+      <form @submit="onSubmit">
+        <input type="text" v-model="title" placeholder="Add Todo...." />
+        <input type="submit" value="submit" />
       </form>
     </div>
   </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex';
+import { mapActions } from "vuex";
 export default {
-  name:'AddTodo',
-  mehods:{
-    ...mapActions(['addTodo'])
-  }
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    ...mapActions(["addTodo"]),
+    onSubmit(){
+      // e.preventDefault();
+      this.addTodo(this.title)
+    }
+  },
 };
 </script>
 
 <style scoped>
-
 form {
   display: flex;
 }
