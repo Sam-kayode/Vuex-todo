a8643dd90054e0593264cfb7fc9279df9cdde600
Sam-kayode <marvelloussamkayode@gmail.com>
2022-02-15 23:19:07 +0100

    add styles for app.vue

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,5 @@ export default {
 };
 </script>
 <style>
-body {
-  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
-  line-height: 1.6;
-  background: #e8f7f0;
-}
-.container {
-  max-width: 1100px;
-  margin: auto;
-  overflow: auto;
-  padding: 0 2rem;
-}
+
 </style>
